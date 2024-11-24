13a66c1043da543518af3db22be6f076e58c8462
Evan Typanski <evantypanski@gmail.com>
2024-11-24 15:21:35 -0500

    Step that stepper

--- stepper.c
+++ stepper.c
@@ -8,7 +8,8 @@ typedef struct {
     Gui *gui;
     ViewPort *view_port;
     FuriMessageQueue *event_queue;
-    const GpioPin *pin;
+    const GpioPin *dirPin;
+    const GpioPin *stepPin;
     bool state;
 } StepperContext;
 
@@ -29,14 +30,15 @@ static void stepper_input_callback(InputEvent* event, void* ctx) {
     furi_message_queue_put(context->event_queue, event, FuriWaitForever);
 }
 
-static StepperContext* stepper_context_alloc(uint8_t pin) {
+static StepperContext* stepper_context_alloc(uint8_t dirPin, uint8_t stepPin) {
   StepperContext* context = malloc(sizeof(StepperContext));
 
   context->view_port = view_port_alloc();
   view_port_draw_callback_set(context->view_port, stepper_draw_callback, context);
   view_port_input_callback_set(context->view_port, stepper_input_callback, context);
   context->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
-  context->pin = furi_hal_resources_pin_by_number(pin)->pin;
+  context->dirPin = furi_hal_resources_pin_by_number(dirPin)->pin;
+  context->stepPin = furi_hal_resources_pin_by_number(stepPin)->pin;
 
   context->gui = furi_record_open(RECORD_GUI);
   gui_add_view_port(context->gui, context->view_port, GuiLayerFullscreen);
@@ -57,13 +59,26 @@ static void stepper_context_free(StepperContext* context) {
 
 int32_t stepper_app(void* p) {
   UNUSED(p);
-  StepperContext *context = stepper_context_alloc(7);
-  furi_hal_gpio_write(context->pin, context->state);
-  furi_hal_gpio_init(context->pin,  GpioModeOutputPushPull, GpioPullNo, GpioSpeedLow);
+  StepperContext *context = stepper_context_alloc(6, 7);
+  furi_hal_gpio_write(context->stepPin, false);
+  furi_hal_gpio_init(context->stepPin,  GpioModeOutputPushPull, GpioPullNo, GpioSpeedLow);
+  furi_hal_gpio_write(context->dirPin, false);
+  furi_hal_gpio_init(context->dirPin,  GpioModeOutputPushPull, GpioPullNo, GpioSpeedLow);
 
   for (bool is_running = true; is_running; ) {
+    // Step before checking anything else
+    if (context->state) {
+      furi_hal_gpio_write(context->stepPin, true);
+      furi_delay_us(3);
+      furi_hal_gpio_write(context->stepPin, false);
+      furi_delay_us(3000);
+    }
+
     InputEvent event;
-    const FuriStatus status = furi_message_queue_get(context->event_queue, &event, FuriWaitForever);
+    // If we're stepping, don't wait
+    uint32_t timeout = context->state ? 0 : FuriWaitForever;
+    const FuriStatus status = furi_message_queue_get(context->event_queue, &event, timeout);
+
     if ((status != FuriStatusOk) || (event.type != InputTypeShort)) {
       continue;
     }
@@ -77,10 +92,10 @@ int32_t stepper_app(void* p) {
       default: break;
     }
 
-    furi_hal_gpio_write(context->pin, context->state);
   }
 
-  furi_hal_gpio_init(context->pin, GpioModeAnalog, GpioPullNo, GpioSpeedLow);
+  furi_hal_gpio_init(context->stepPin, GpioModeAnalog, GpioPullNo, GpioSpeedLow);
+  furi_hal_gpio_init(context->dirPin, GpioModeAnalog, GpioPullNo, GpioSpeedLow);
   stepper_context_free(context);
 
   return 0;
