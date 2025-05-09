8e0804e85e5ab7a280021ccaa21b58b2c1684dc3
Evan Typanski <evantypanski@gmail.com>
2025-05-09 15:45:58 -0400

    Add choice for step/dir pins

--- stepper.c
+++ stepper.c
@@ -8,17 +8,35 @@
 typedef enum {
   ViewIndexSubmenu,
   ViewIndexStepping,
+  ViewIndexStepPin,
+  ViewIndexDirPin,
 } ViewIndex;
 
 // Enumeration of submenu items.
 typedef enum {
   SubmenuIndexStep,
+  SubmenuIndexStepPin,
+  SubmenuIndexDirPin,
 } SubmenuIndex;
+
+// Each pin that can be used
+typedef enum {
+  PinA7,
+  PinA6,
+  PinA4,
+  PinB3,
+  PinB2,
+  PinC3,
+  PinC1,
+  PinC0,
+} UsablePins;
 
 typedef struct {
   Gui *gui;
   ViewDispatcher *view_dispatcher;
   Submenu *submenu;
+  Submenu *stepmenu;
+  Submenu *dirmenu;
   Widget *stepping_widget;
   const GpioPin *dirPin;
   const GpioPin *stepPin;
@@ -52,8 +70,8 @@ static bool stepper_event_callback(void *ctx, uint32_t event) {
 
   if (event == ViewIndexStepping) {
     FURI_LOG_D("stepper", "Changing to stepping");
-    // view_dispatcher_set_tick_event_callback(context->view_dispatcher,
-    // stepper_tick_callback, 3000);
+    view_dispatcher_set_tick_event_callback(context->view_dispatcher,
+                                            stepper_tick_callback, 3000);
 
     furi_event_loop_tick_set(
         view_dispatcher_get_event_loop(context->view_dispatcher), 3,
@@ -77,7 +95,59 @@ static void stepper_dispatcher_app_submenu_callback(void *ctx, uint32_t index) {
   if (index == SubmenuIndexStep) {
     view_dispatcher_send_custom_event(context->view_dispatcher,
                                       ViewIndexStepping);
+  } else if (index == SubmenuIndexStepPin) {
+    view_dispatcher_send_custom_event(context->view_dispatcher,
+                                      ViewIndexStepPin);
+  } else if (index == SubmenuIndexDirPin) {
+    view_dispatcher_send_custom_event(context->view_dispatcher,
+                                      ViewIndexDirPin);
   }
+}
+
+static void switch_pin(StepperContext *context, const GpioPin **pin,
+                       uint32_t index) {
+  furi_hal_gpio_init(*pin, GpioModeAnalog, GpioPullNo, GpioSpeedLow);
+  switch (index) {
+  case PinA7:
+    *pin = furi_hal_resources_pin_by_number(2)->pin;
+    break;
+  case PinA6:
+    *pin = furi_hal_resources_pin_by_number(3)->pin;
+    break;
+  case PinA4:
+    *pin = furi_hal_resources_pin_by_number(4)->pin;
+    break;
+  case PinB3:
+    *pin = furi_hal_resources_pin_by_number(5)->pin;
+    break;
+  case PinB2:
+    *pin = furi_hal_resources_pin_by_number(6)->pin;
+    break;
+  case PinC3:
+    *pin = furi_hal_resources_pin_by_number(7)->pin;
+    break;
+  case PinC1:
+    *pin = furi_hal_resources_pin_by_number(15)->pin;
+    break;
+  case PinC0:
+    *pin = furi_hal_resources_pin_by_number(16)->pin;
+    break;
+  }
+  furi_hal_gpio_write(*pin, false);
+  furi_hal_gpio_init(*pin, GpioModeOutputPushPull, GpioPullNo, GpioSpeedLow);
+  view_dispatcher_send_custom_event(context->view_dispatcher, ViewIndexSubmenu);
+}
+
+static void stepper_dispatcher_step_pin_callback(void *ctx, uint32_t index) {
+  furi_assert(ctx);
+  StepperContext *context = ctx;
+  switch_pin(context, &context->stepPin, index);
+}
+
+static void stepper_dispatcher_dir_pin_callback(void *ctx, uint32_t index) {
+  furi_assert(ctx);
+  StepperContext *context = ctx;
+  switch_pin(context, &context->stepPin, index);
 }
 
 static void stepping_button_callback(GuiButtonType button_type,
@@ -92,6 +162,18 @@ static void stepping_button_callback(GuiButtonType button_type,
   }
 }
 
+static void add_pins_to_submenu(StepperContext *context, Submenu *submenu,
+                                SubmenuItemCallback callback) {
+  submenu_add_item(submenu, "A7", PinA7, callback, context);
+  submenu_add_item(submenu, "A6", PinA6, callback, context);
+  submenu_add_item(submenu, "A4", PinA4, callback, context);
+  submenu_add_item(submenu, "B3", PinB3, callback, context);
+  submenu_add_item(submenu, "B2", PinB2, callback, context);
+  submenu_add_item(submenu, "C3", PinC3, callback, context);
+  submenu_add_item(submenu, "C1", PinC1, callback, context);
+  submenu_add_item(submenu, "C0", PinC0, callback, context);
+}
+
 static StepperContext *stepper_context_alloc(uint8_t dirPin, uint8_t stepPin) {
   StepperContext *context = malloc(sizeof(StepperContext));
 
@@ -102,8 +184,22 @@ static StepperContext *stepper_context_alloc(uint8_t dirPin, uint8_t stepPin) {
 
   // Create and initialize the Submenu view.
   context->submenu = submenu_alloc();
-  submenu_add_item(context->submenu, "Start Stepping", SubmenuIndexStep,
+  submenu_add_item(context->submenu, "Go!", SubmenuIndexStep,
                    stepper_dispatcher_app_submenu_callback, context);
+  submenu_add_item(context->submenu, "Step Pin", SubmenuIndexStepPin,
+                   stepper_dispatcher_app_submenu_callback, context);
+  submenu_add_item(context->submenu, "Dir Pin", SubmenuIndexDirPin,
+                   stepper_dispatcher_app_submenu_callback, context);
+
+  // Step pin submenu
+  context->stepmenu = submenu_alloc();
+  add_pins_to_submenu(context, context->stepmenu,
+                      stepper_dispatcher_step_pin_callback);
+
+  // Dir pin submenu
+  context->dirmenu = submenu_alloc();
+  add_pins_to_submenu(context, context->dirmenu,
+                      stepper_dispatcher_dir_pin_callback);
 
   // Create the stepping widget.
   context->stepping_widget = widget_alloc();
@@ -118,6 +214,10 @@ static StepperContext *stepper_context_alloc(uint8_t dirPin, uint8_t stepPin) {
                                 ViewDispatcherTypeFullscreen);
   view_dispatcher_add_view(context->view_dispatcher, ViewIndexSubmenu,
                            submenu_get_view(context->submenu));
+  view_dispatcher_add_view(context->view_dispatcher, ViewIndexStepPin,
+                           submenu_get_view(context->stepmenu));
+  view_dispatcher_add_view(context->view_dispatcher, ViewIndexDirPin,
+                           submenu_get_view(context->dirmenu));
   view_dispatcher_add_view(context->view_dispatcher, ViewIndexStepping,
                            widget_get_view(context->stepping_widget));
   view_dispatcher_set_custom_event_callback(context->view_dispatcher,
@@ -138,8 +238,12 @@ static StepperContext *stepper_context_alloc(uint8_t dirPin, uint8_t stepPin) {
 
 static void stepper_context_free(StepperContext *context) {
   view_dispatcher_remove_view(context->view_dispatcher, ViewIndexSubmenu);
+  view_dispatcher_remove_view(context->view_dispatcher, ViewIndexStepPin);
+  view_dispatcher_remove_view(context->view_dispatcher, ViewIndexDirPin);
   view_dispatcher_remove_view(context->view_dispatcher, ViewIndexStepping);
   view_dispatcher_free(context->view_dispatcher);
+  submenu_free(context->stepmenu);
+  submenu_free(context->dirmenu);
   submenu_free(context->submenu);
   furi_record_close(RECORD_GUI);
   free(context);
